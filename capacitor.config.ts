4c175030b5e3067f775f1d79fb20227135d91e4a
Alerop <alejandrorp93@gmail.com>
2025-03-27 16:58:48 +0100

    making only a only without being app

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Only',
-  webDir: 'www'
-};
-
-export default config;
